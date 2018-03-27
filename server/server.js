4d24d7c10a109ed1270d239ad87c5ad5a89d76b2
Rob Ball <rob@coolbreeze.co.uk>
2018-03-27 16:47:22 +0100

    Welcome new user and alert other users

--- server/server.js
+++ server/server.js
@@ -14,21 +14,32 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
    console.log('New user connected');
 
-// Emits event to single connection
-   // socket.emit('newMessage', {
-   //    from: 'rob',
-   //    text: 'Hello',
-   //    createdAt: 123
-   // });
+   socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app!',
+      createdAt: new Date().getTime()
+   });
+   socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New joiner',
+      createdAt: new Date().getTime()
+   })
 
+// Listening for createMessage event
    socket.on('createMessage', (message) => {
       console.log('New message', message);
 // Emits event to every connection
-      io.emit('newMessage', {
-         from: message.from,
-         text: message.text,
-         createdAt: new Date().getTime()
-      })
+// io.emit('newMessage', {
+//    from: message.from,
+//    text: message.text,
+//    createdAt: new Date().getTime()
+// })
+// Emits event to every user apart from the sender
+socket.broadcast.emit('newMessage', {
+   from: message.from,
+   text: message.text,
+   createdAt: new Date().getTime()
+})
    });
 
    socket.on('disconnect', () => {
