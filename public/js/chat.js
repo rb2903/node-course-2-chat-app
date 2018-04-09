c30c4c19c45175df92be13beb16814f1d1030515
Rob Ball <rob@coolbreeze.co.uk>
2018-04-09 17:35:45 +0100

    Add join page that submits to chat.html

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,20 @@
 var socket = io();
+
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+     messages.scrollTop(scrollHeight);
+  }
+}
 socket.on('connect', function() {
    console.log('Connected to server');
 });
@@ -16,6 +32,7 @@ socket.on('newMessage', function(message) {
       createdAt: formattedTime
    });
    jQuery('#messages').append(html);
+   scrollToBottom();
    // var li = jQuery('<li></li>');
    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
    // jQuery('#messages').append(li);
@@ -32,6 +49,7 @@ socket.on('newLocationMessage', function (message) {
       url: message.url
    });
    jQuery('#messages').append(html);
+   scrollToBottom();
 });
 
 // No longer need this as we have a form to submit messages
